af175283440c7562849edd7245e1d1a59271e8de
Van Doan <vdoan09@gmail.com>
2015-01-04 22:36:14 -0500

    extend

---
+++ objectMethod.php
@@ -0,0 +1,35 @@
+<html><html>
+  <head>
+    <title>Class and Object Methods</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        class Person {
+          public $isAlive = true;
+          
+          function __construct($name) {
+              $this->name = $name;
+          }
+          
+          public function dance() {
+            return "I'm dancing!";
+          }
+        }
+        
+        $me = new Person("Shane");
+        if (is_a($me, "Person")) {
+          // is_a finds if it's an instance of a certain class
+    
+          echo "I'm a person, ";
+        }
+        if (property_exists($me, "name")) {
+          echo "I have a name, ";
+        }
+        if (method_exists($me, "dance")) {
+          echo "and I know how to dance!";
+        }
+      ?>
+    </p>
+  </body>
+</html></html>
