5369ce41fcd7cda2df50100c5e7bc3fe412618bd
Van Doan <vdoan09@gmail.com>
2015-01-06 23:50:13 -0500

    associative array and more

---
+++ fruitArray.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Array Review</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $fruits = array('bananas', 'apples', 'pears');               /* Your code here! */
+        echo 'I love eating ' . $fruits[1] . /* Your code here! */  ' too!';
+      ?>
+    </p>
+  </body>
+</html>
